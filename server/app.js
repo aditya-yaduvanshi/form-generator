64f92f4cace041dab5c8d25756928058cd29aee2
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2023-12-08 11:35:05 +0530

    allow list updated

--- server/app.js
+++ server/app.js
@@ -9,28 +9,27 @@ const express = require("express"),
   PORT = process.env.PORT || 5000,
   HOST = process.env.HOST || "0.0.0.0",
   MONGO_URI = process.env.MONGO_URI,
-  {forms, responses} = require("./routes");
+  { forms, responses } = require("./routes");
 
 app.use(
   cors((req, callback) => {
     let corsOptions,
-      allowlist = [
-        "https://formsheet.netlify.app",
-        "https://formsheat.herokuapp.com",
-      ];
+      allowlist = ["https://formsheet.up.railway.app"];
     if (allowlist.indexOf(req.header("Origin")) !== -1) {
-      corsOptions = {origin: true};
+      corsOptions = { origin: true };
     } else {
-      corsOptions = {origin: false};
+      corsOptions = { origin: false };
     }
     callback(null, corsOptions);
   })
 );
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 },
-  createParentPath: true,
-}));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    createParentPath: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/forms", forms);
 app.use("/api/responses", responses);
@@ -47,9 +46,7 @@ mongoose
   })
   .then((_conn) => {
     console.log("db connected");
-    app.listen(PORT, HOST, () =>
-      console.log("listening on port", PORT)
-    );
+    app.listen(PORT, HOST, () => console.log("listening on port", PORT));
   })
   .catch((err) => {
     console.error(err);
